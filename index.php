890e9b6d6dd3f04b5146135d3455a76534b90c8f
rvdforst <rolf.v.d.forst@fau.de>
2021-04-19 09:26:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace RRZE\Mjml;
+
+require __DIR__ . '/vendor/autoload.php';
+
+Router::add('/', function () {
+    echo 'RRZE Mjml Server';
+});
+
+Router::add('/v1', function () {
+    $bin = __DIR__ . '/node_modules/.bin/mjml';
+    $cacheDir = __DIR__ . '/cache';
+    (new Render($bin))->run();
+}, 'post');
+
+Router::run('/');
