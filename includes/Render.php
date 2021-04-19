d2a66574114078caad5d0520e565ba6c6b5e1847
rvdforst <rolf.v.d.forst@fau.de>
2021-04-19 09:26:41 +0200

    Create Render.php

---
+++ includes/Render.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace RRZE\Mjml;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class Render
+{
+    private $httpRequest;
+
+    protected $bin;
+
+    public function __construct(string $bin)
+    {
+        $this->bin = $bin;
+        $this->httpRequest = new HttpRequest();
+    }
+
+    public function run(): void
+    {
+        $body = $this->httpRequest->body();
+        $bodyObj = json_decode($body);
+        $content = $bodyObj->mjml;
+
+        echo $this->render($content);
+    }
+
+    protected function render(string $content): string
+    {
+        $args = [
+            $this->bin,
+            '-i',
+            '-s',
+            '--config.beautify'
+        ];
+
+        $process = new Process($args);
+        $process->setInput($content);
+
+        try {
+            $process->mustRun();
+        } catch (ProcessFailedException $e) {
+            throw new \RuntimeException('Unable to transpile MJML. Stack error: ' . $e->getMessage());
+        }
+
+        $html = $process->getOutput();
+
+        return json_encode([
+            'html' => $this->stripFirstComment($html)
+        ]);
+    }
+
+    protected function stripFirstComment($html)
+    {
+        $pos = strpos($html, '<!--');
+        $_pos = strpos($html, '-->');
+        if ($pos === 0 && $_pos !== false) {
+            $html = substr($html, $_pos + 3);
+        }
+        return $html;
+    }
+}
