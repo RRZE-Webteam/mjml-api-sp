c2b12761462cd495be142996cac3ff92639c9da5
rvdforst <rolf.v.d.forst@fau.de>
2021-04-28 09:25:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,27 +7,24 @@ use Monolog\Handler\StreamHandler;
 use studio24\Rotate\Rotate;
 use studio24\Rotate\Delete;
 
-const CACHE = __DIR__ . '/cache';
+const CACHE_DIR = __DIR__ . '/cache';
 
-const LOG = __DIR__ . '/log';
+const LOG_DIR = __DIR__ . '/log';
 
-if (!is_dir(CACHE)) {
-    mkdir(CACHE, 0_777, true);
+date_default_timezone_set('UTC');
+
+if (!is_dir(CACHE_DIR)) {
+    mkdir(CACHE_DIR, 0_777, true);
 }
 
-if (!is_dir(LOG)) {
-    mkdir(LOG, 0_777, true);
+if (!is_dir(LOG_DIR)) {
+    mkdir(LOG_DIR, 0_777, true);
 }
-
-if (!defined('DEFAULT_TIMEZONE')) {
-    define('DEFAULT_TIMEZONE', 'UTC');
-}
-date_default_timezone_set(DEFAULT_TIMEZONE);
 
 require __DIR__ . '/vendor/autoload.php';
 
 $log = new Logger('RRZE-MJML');
-$log->pushHandler(new StreamHandler(LOG . '/error.log', Logger::ERROR));
+$log->pushHandler(new StreamHandler(LOG_DIR . '/error.log', Logger::ERROR));
 
 $configSample = __DIR__ . '/config-sample.php';
 $config = __DIR__ . '/config.php';
@@ -41,15 +38,15 @@ require $config;
 
 $log->error('Cannot config-sample.php file to config.php file.', ['config file' => $config]);
 
-$rotate = new Rotate(LOG . '/*.log');
+$rotate = new Rotate(LOG_DIR . '/*.log');
 $rotate->size('1MB');
 $rotate->run();
 
-$rotate = new Delete(CACHE . '/*.html');
+$rotate = new Delete(CACHE_DIR . '/*.html');
 $rotate->deleteByFileModifiedDate('1 hour');
 
-$rotate = new Delete(CACHE . '/*.mjml');
-$rotate->deleteByFileModifiedDate('1 hour');
+$rotate = new Delete(CACHE_DIR . '/*.mjml');
+$rotate->deleteByFileModifiedDate('1 minute');
 
 Router::add('/', function () {
     echo 'MJML API Service Provider';
